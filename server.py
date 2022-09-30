7a4f6eaa4a808e6e8a0ae6ca0c0484c4d70d6042
KierMc <kiernanmcvay@gmail.com>
2022-09-30 10:37:43 -0700

    first commit - Checkerboard

---
+++ server.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/<int:x>')
+@app.route('/<int:x>/<int:y>')
+@app.route('/<int:x>/<int:y>')
+def checker (x=8, y=8):
+    return render_template ('index.html', row=x, col=y)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
